8c3ea4ddc25eb2b4cd55a336c92a52e04607e69f
Moussa FOFANA <didegassama@gmail.com>
2025-09-19 13:19:05 +0000

    fix vite config and remove react page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
-import ghPages from "vite-plugin-gh-pages";
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ghPages(),
     VitePWA({
       registerType: "prompt",
       includeAssets: ["favicon.ico", "apple-touch-icon.png"],
